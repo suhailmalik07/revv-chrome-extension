d50b6b3db714e4ce10e7348c854d84cf3c519126
Omkarnath Parida <pomkarnath98@gmail.com>
2020-09-17 23:40:34 +0530

    Updated

--- JS/index.js
+++ JS/index.js
@@ -170,85 +170,87 @@ goToActivity.addEventListener("click", toggleToActivity);
 const goToRecent = document.getElementById("goToRecent");
 goToRecent.addEventListener("click", toggleToRecent);
 
-// const btn = document.getElementById("loginBtn")
-// const root = document.getElementById("root")
-// let user = {}
+const btn = document.getElementById("loginBtn")
+const root = document.getElementById("root")
+let user = {}
 
 // document.addEventListener("DOMContentLoaded", function () {
 //     renderDOM()
 // })
 
-// function renderDOM() {
-//     loadData()
-//     !user?.access_token ? loadForm() : renderApp()
-// }
+function renderDOM() {
+    loadData()
+    !user?.access_token ? loadForm() : renderApp()
+}
 
-// const loadData = () => {
-//     console.log(chrome.storage)
-//     chrome.storage.local.get(["user"], function (items) {
-//         user = JSON.parse(items["user"] || "{}")
-//         renderDOM()
-//     })
-// }
+const loadData = () => {
+    console.log(chrome.storage)
+    chrome.storage.local.get(["user"], function (items) {
+        user = JSON.parse(items["user"] || "{}")
+        renderDOM()
+    })
+}
 
-// function renderApp() {
-//     root.innerHTML = user
-// }
+function renderApp() {
+    root.innerHTML = user
+}
+//  value="coldforce+revvhack@gmail.com"
+//  value="Coldforce@1"
+//  value="ftal37a3"
+function loadForm() {
+    root.innerHTML = `<form id="loginForm">
+        <input type="email" name="email" placeholder="Email" >
+        <input type="password" name="Password" id="" placeholder="Password" >
+        <input type="text" name="org_domain" placeholder="org_domain" name="org_domain">
+        <button id="loginBtn">Login</button>
+    </form>`
 
-// function loadForm() {
-//     root.innerHTML = `<form id="loginForm">
-//         <input type="email" value="coldforce+revvhack@gmail.com" name="email" placeholder="Email" >
-//         <input type="password" value="Coldforce@1" name="password" id="" placeholder="Password" >
-//         <input type="text" value="ftal37a3" name="org_domain" placeholder="org_domain" name="org_domain">
-//         <button id="loginBtn">Login</button>
-//     </form>`
+    const loginForm = document.getElementById("loginForm")
+    loginForm.onsubmit = e => {
+        e.preventDefault()
+        handleLogin(loginForm)
+    }
+}
 
-//     const loginForm = document.getElementById("loginForm")
-//     loginForm.onsubmit = e => {
-//         e.preventDefault()
-//         handleLogin(loginForm)
-//     }
-// }
+const handleLogin = async (form) => {
+    const data = new FormData(form)
+    const payload = { user_email: data.get("email"), password: data.get("password"), org_domain: data.get("org_domain") }
 
-// const handleLogin = async (form) => {
-//     const data = new FormData(form)
-//     const payload = { user_email: data.get("email"), password: data.get("password"), org_domain: data.get("org_domain") }
+    for (let key in payload) {
+        if (!payload[key]) {
+            return
+        }
+    }
 
-//     for (let key in payload) {
-//         if (!payload[key]) {
-//             return
-//         }
-//     }
+    const url = `https:/api.revvsales.com/api/v2/auth/initiate-auth`
+    const { User } = await postData(url, payload)
+    user = User
+    chrome.storage.local.set({ "user": JSON.stringify(user) })
+    chrome.storage.local.get("user", (item) => {
+        console.log(item)
+    })
 
-//     const url = `https:/api.revvsales.com/api/v2/auth/initiate-auth`
-//     const { User } = await postData(url, payload)
-//     user = User
-//     chrome.storage.local.set({ "user": JSON.stringify(user) })
-//     chrome.storage.local.get("user", (item) => {
-//         console.log(item)
-//     })
+    renderDOM()
+}
 
-//     renderDOM()
-// }
-
-// const postData = async (url = "", data = {}) => {
-//     try {
-//         const response = await fetch(url, {
-//             method: "POST",
-//             body: JSON.stringify(data),
-//             mode: 'cors', // no-cors, *cors, same-origin
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 'GrantType': 'password'
-//                 // 'Content-Type': 'application/x-www-form-urlencoded',
-//             }
-//         })
-//         return response.json()
-//     }
-//     catch (err) {
-//         console.log(err)
-//     }
-// }
+const postData = async (url = "", data = {}) => {
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(data),
+            mode: 'cors', // no-cors, *cors, same-origin
+            headers: {
+                'Content-Type': 'application/json',
+                'GrantType': 'password'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            }
+        })
+        return response.json()
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
 
 
 function setNotification(count) {
