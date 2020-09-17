3d1efc03135bba28b1d75ffaaa60ac3391f018c0
Suhail Malik <devmsuhail@gmail.com>
2020-09-18 00:39:35 +0530

    Added Login feature

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,5 @@
+let user;
+
 const getDocs = () => {
   chrome.storage.local.get("docs", (data) => {
     const { docs } = data;
@@ -19,131 +21,23 @@ const getActivities = () => {
   })
 }
 
-window.addEventListener("load", () => {
-  getDocs();
-  getActivities();
-});
 
-const findCurrentTime = (timestamp) => {
-  var d = new Date(timestamp * 1000),
-    yyyy = d.getFullYear(),
-    mm = ("0" + (d.getMonth() + 1)).slice(-2),
-    dd = ("0" + d.getDate()).slice(-2),
-    hh = d.getHours(),
-    h = hh,
-    min = ("0" + d.getMinutes()).slice(-2),
-    ampm = "AM",
-    time;
-  if (hh > 12) {
-    h = hh - 12;
-    ampm = "PM";
-  } else if (hh === 12) {
-    h = 12;
-    ampm = "PM";
-  } else if (hh == 0) {
-    h = 12;
+function handleClickOnRecent(e) {
+  const { name, id } = e.target
+  if (name == "gen_parmaLink" && id) {
+    fetch("https://api.revvsales.com/api/perma-link", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'AccessToken': 'access_token'
+      },
+      body: {
+        "object_id": id,
+        "object_type": "DOC"
+      }
+    })
   }
-  time = yyyy + "-" + mm + "-" + dd + ", " + h + ":" + min + " " + ampm;
-  return time;
-};
-
-const displayActivity = (data) => {
-  const activityTable = document.getElementById("activity-table");
-  data.map((e) => {
-    const activityList = document.createElement("tr");
-    activityList.className = "activity-list";
-    switch (e.event) {
-      case "ACCEPTANCE_EMAIL_DELIVERED":
-        var timestamp = Math.round(Date.parse(e.timestamp) / 1000);
-        activityList.innerHTML = `<td>
-                    <img src='../Resources/activity.png' alt='document'/>
-                </td>
-                <td>
-                    <div><b>${e.actor.name}</b><img src=${
-          e.actor.image_url
-        } alt='profile_img'/></div>
-                    <div><b>${
-                      e.actor.name
-                    }</b> received the email & document for acceptance</div>
-                    <div><i>${findCurrentTime(timestamp)}</i></div>
-                </td>
-                `;
-        break;
-        case "DOCUMENT_SENT_FOR_ACCEPT":
-        var timestamp = Math.round(Date.parse(e.timestamp) / 1000);
-        let arr = []
-        e.subjects.map(ele=>{
-            arr.push(ele.name)
-        })
-        activityList.innerHTML = `<td>
-                    <img src='../Resources/activity.png' alt='document'/>
-                </td>
-                <td>
-                    <div><b>${e.actor.name}</b><img src=${
-          e.actor.image_url
-        } alt='profile_img'/></div>
-                    <div>Document sent for acceptance to <b>${arr.join(', ')}</b></div>
-                    <div><i>${findCurrentTime(timestamp)}</i></div>
-                </td>
-                `;
-        break;
-        case "DOCUMENT_CREATED":
-        var timestamp = Math.round(Date.parse(e.timestamp) / 1000);
-        activityList.innerHTML = `<td>
-                    <img src='../Resources/activity.png' alt='document'/>
-                </td>
-                <td>
-                    <div><b>${e.actor.name}</b><img src=${
-          e.actor.image_url
-        } alt='profile_img'/></div>
-                    <div><b>Congrats!</b> A new document created successfully</div>
-                    <div><i>${findCurrentTime(timestamp)}</i></div>
-                </td>
-                `;
-        break;
-        default:
-            activityList.innerHTML = `<td>
-            <img src='../Resources/activity.png' alt='document'/>
-        </td>
-        <td>Something Happened magically!</td>`
-    }
-    activityTable.append(activityList);
-  });
-};
-
-const displayData = (docs) => {
-  const recentTable = document.getElementById("recent-table");
-  console.log(docs);
-  docs.map((e) => {
-    const recentList = document.createElement("tr");
-    recentList.className = "recent-list";
-    recentList.innerHTML = `<td class='recent-items'>
-                <img src='../Resources/document.png' alt='document'/>
-            </td>
-            <td>
-                <div class='docName'>
-                    <b>${e.docName}</b>
-                </div>
-
-                <div>
-                    <i>Doc No: ${e.docId}</i>
-                </div>
-
-            </td>
-            <td>
-                <a target='_blank' href=${e.url}><button>Open</button></a>
-            </td>            
-            
-            <td class='recent-dir'>
-                <button>Generate Link</button>
-                <div>
-                    Directory: ${e.dir ? e.dir : "/"}
-                </div>
-            </td>
-            `;
-    recentTable.append(recentList);
-  });
-};
+}
 
 const toggleToActivity = () => {
   document.getElementById("activity").style.display = "inline";
@@ -169,6 +63,42 @@ goToActivity.addEventListener("click", toggleToActivity);
 
 const goToRecent = document.getElementById("goToRecent");
 goToRecent.addEventListener("click", toggleToRecent);
+
+
+function renderDOM(user) {
+  !user.access_token ? showForm() : showApp()
+}
+
+function showApp() {
+  document.getElementById("app").style.display = "block"
+  document.getElementById("form").style.display = "none"
+
+  getDocs();
+  getActivities();
+}
+
+function showForm() {
+  document.getElementById("app").style.display = "none"
+  document.getElementById("form").style.display = "block"
+
+  // add event listener to form
+  document.getElementById("form").addEventListener("submit", async e => {
+    e.preventDefault()
+    try {
+      user = await handleLogin(e.target)
+      console.log(user)
+      renderDOM(user)
+    } catch (err) {
+      console.log(err)
+    }
+  })
+}
+
+chrome.storage.local.get("user", (data) => {
+  user = data?.user || {}
+  console.log(user, "user")
+  renderDOM(user)
+})
 
 // const btn = document.getElementById("loginBtn")
 // const root = document.getElementById("root")
@@ -197,10 +127,7 @@ goToRecent.addEventListener("click", toggleToRecent);
 
 // function loadForm() {
 //     root.innerHTML = `<form id="loginForm">
-//         <input type="email" value="coldforce+revvhack@gmail.com" name="email" placeholder="Email" >
-//         <input type="password" value="Coldforce@1" name="password" id="" placeholder="Password" >
-//         <input type="text" value="ftal37a3" name="org_domain" placeholder="org_domain" name="org_domain">
-//         <button id="loginBtn">Login</button>
+//         
 //     </form>`
 
 //     const loginForm = document.getElementById("loginForm")
@@ -210,45 +137,6 @@ goToRecent.addEventListener("click", toggleToRecent);
 //     }
 // }
 
-// const handleLogin = async (form) => {
-//     const data = new FormData(form)
-//     const payload = { user_email: data.get("email"), password: data.get("password"), org_domain: data.get("org_domain") }
-
-//     for (let key in payload) {
-//         if (!payload[key]) {
-//             return
-//         }
-//     }
-
-//     const url = `https:/api.revvsales.com/api/v2/auth/initiate-auth`
-//     const { User } = await postData(url, payload)
-//     user = User
-//     chrome.storage.local.set({ "user": JSON.stringify(user) })
-//     chrome.storage.local.get("user", (item) => {
-//         console.log(item)
-//     })
-
-//     renderDOM()
-// }
-
-// const postData = async (url = "", data = {}) => {
-//     try {
-//         const response = await fetch(url, {
-//             method: "POST",
-//             body: JSON.stringify(data),
-//             mode: 'cors', // no-cors, *cors, same-origin
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 'GrantType': 'password'
-//                 // 'Content-Type': 'application/x-www-form-urlencoded',
-//             }
-//         })
-//         return response.json()
-//     }
-//     catch (err) {
-//         console.log(err)
-//     }
-// }
 
 
 function setNotification(count) {
